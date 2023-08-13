5d4e3e2ac11a0d2456fb3b4cb199d1dc6b213b46
Daxwill <74215083+Daxwill@users.noreply.github.com>
2023-08-13 20:19:46 -0300

    changes HTML divs ,add CSS , add logica errores y msj

--- practico01/js/script.js
+++ practico01/js/script.js
@@ -1,15 +1,18 @@
 function calcular() {
+    //Ingreso de datos
     var n1 = document.getElementById("n1").value;
-    n1 = parseInt(n1);
+    n1 = parseFloat(n1);
     var n2 = document.getElementById("n2").value;
-    n2 = parseInt(n2);
+    n2 = parseFloat(n2);
 
     // Validacion de numeros
     var msjError1 = "Ingrese un numero valido."
     if (isNaN(n1)) {
         alert(msjError1);
+        return;
     } else if (isNaN(n2)) {
         alert(msjError1);
+        return;
     }
 
     // Validacion de operaciones
@@ -17,6 +20,14 @@ function calcular() {
     var operador = document.getElementById("operaciones").value;
     if (operador === "null") {
         alert(msjError2);
+        return;
+    }
+
+    // Manejo de errores
+    var msjError3 = "No es valido dividir entre 0"
+    if (operador === "div" && n2 === 0) {
+        alert(msjError3)
+        return;
     }
 
     // Calculos
@@ -35,8 +46,29 @@ function calcular() {
             resultado = n1 / n2;
             break;
     }
+
+    // manejo de respuestas
+    var msjResultado;
+    if (resultado > Math.pow(10, 10)) {
+        msjResultado = "El resultado es demasiado grande"
+        alert(msjResultado);
+        return;
+    } else if (resultado < Math.pow(10, -10)) {
+        msjResultado = "El resultado es demasiado pequeño"
+        alert(msjResultado);
+        return;
+    }
+
+    // Devolucion de respuesta
     var msjResultado = `El resultado es ${resultado}.`;
     return document.getElementById("resultado").innerHTML = msjResultado;
 }
 
+// funcion para refrescar la pagina
+function recargar() {
+    location.reload();
+}
+
+// llamado a la funcion
 document.getElementById("botonCalc").addEventListener("click", calcular);
+document.getElementById("botonReset").addEventListener("click", recargar);
